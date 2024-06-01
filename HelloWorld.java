4faca5d344b27f27fb816b6e6439afb8cfc42c5f
bibhishandhagate <bibhishandhagate@gmail.com>
2024-06-01 23:00:35 +0530

    HelloWorld Java program

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Bibhishan Dhagate! keep learning");
+		System.out.println("This is the beauty of the Jenkins Tool");
+    }
+}
