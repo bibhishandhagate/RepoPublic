c1aff24707b2424c3a4eeda4064e2cec3af4def8
bibhishandhagate <bibhishandhagate@gmail.com>
2024-06-02 12:07:54 +0530

    pushed changes to verify poll scm

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Bibhishan Dhagate! keep learning");
 		System.out.println("This is the beauty of the Jenkins Tool");
+		System.out.println("This change is to test the Poll SCM");
     }
 }
